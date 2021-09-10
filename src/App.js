c57074afaf19739818200c88b7e252317a1b1bbe
Temitope Ogunleye <topeogunleye1@gmail.com>
2021-09-10 21:38:39 +0100

    reformats app.js with prettier

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
 import Navbar from './Navbar';
-import Home from './Home'
+import Home from './Home';
 
 function App() {
-
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar />
       <div className="content">
-        <Home/>
+        <Home />
       </div>
     </div>
   );
