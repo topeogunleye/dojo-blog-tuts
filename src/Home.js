162049d5afc21c29ca9deb9a9ca7bf3b441501ca
Temitope Ogunleye <topeogunleye1@gmail.com>
2021-09-11 13:48:41 +0100

    adds new hooks to home component

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,18 @@
+import { useState } from 'react';
+
 const Home = () => {
+  const [name, setName] = useState('Mario');
+  const [age, setAge] = useState('25');
 
   const handleClick = () => {
-    console.log('hello, ninjas')
-  }
+    setName('Ben');
+    setAge(30)
+  };
+
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <p>{name} is {age} years old</p>
       <button onClick={handleClick}>Click Me</button>
     </div>
   );
