afc6e33a25669851d57d8efabe10078dc10a320c
Temitope Ogunleye <topeogunleye1@gmail.com>
2021-09-22 18:02:10 +0100

    removed blogs state and useEffect

--- src/Home.js
+++ src/Home.js
@@ -2,34 +2,25 @@ import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    {
-      title: 'Web dev top tips',
-      body: 'lorem ipsum...',
-      author: 'mario',
-      id: 3,
-    },
-  ]);
-
-  const [name, setName] = useState('mario')
+ 
 
   const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog => blog.id !== id)
-    setBlogs(newBlogs)
-  }
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  };
 
-  useEffect(() => {
-    console.log('use effect ran')
-    console.log(name)
-  }, [name])
-
+  
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
-      <button onClick={() => setName('luigi')}>change name</button>
-      <p>{name}</p>
+      { error && <div>{ error }</div>}
+      {isPending && <div>Loading...</div>}
+      {blogs && (
+        <BlogList
+          blogs={blogs}
+          title="All Blogs!"
+          handleDelete={handleDelete}
+        />
+      )}
     </div>
   );
 };
