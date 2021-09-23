1cec9c2961ff2df4b7387ded7e3bcafc8d71db8e
Temitope Ogunleye <topeogunleye1@gmail.com>
2021-09-23 15:24:25 +0100

    adds useFetch hook to Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,10 @@
 import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 const Home = () => {
  
-  const useFetch = {}
+  const {data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs')
 
   
   return (
