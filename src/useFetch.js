d65d0b5320cc85efd50ae0a772b97d5672205063
Temitope Ogunleye <topeogunleye1@gmail.com>
2021-09-23 15:25:30 +0100

    exports data, isPending, and error from useFetch hoo

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,13 +1,13 @@
 import { useState,useEffect } from "react";
 
-const useFetch = () => {
-  const [blogs, setBlogs] = useState(null);
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
   const [isPending, setIsPending] = useState(true);
   const [error, setError] = useState(null)
 
   useEffect(() => {
     setTimeout(() => {
-      fetch('http://localhost:8000/blogs')
+      fetch(url)
         .then((res) => {
           if (!res.ok) {
             throw Error('could not fetch the data for that resource')
@@ -15,17 +15,17 @@ const useFetch = () => {
           return res.json();
         })
         .then((data) => {
-          // console.log(data);
-          setBlogs(data);
+          setData(data);
           setIsPending(false);
           setError(null)
         })
         .catch(err => {
-          // console.log(err.message)
           setError(err.message)
         })
     }, 4000);
-  }, []);
+  }, [url]);
+
+  return {data, isPending, error}
 }
  
 export default useFetch;
